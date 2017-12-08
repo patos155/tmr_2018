3468925345643569bb5e6a6929e64d7cd39a1322
CharsMtz <charley201050@hotmail.com>
2017-12-07 18:13:52 -0600

    Linea

--- Seguidor de Línea/Linea_v1/Linea_v1.ino
+++ Seguidor de Línea/Linea_v1/Linea_v1.ino
@@ -1,18 +1,18 @@
 #include <AFMotor.h>
-AF_DCMotor Md(4);   //motor derecho
+AF_DCMotor Md(2);   //motor derecho
 AF_DCMotor Mi(3);   //motor izquierdo
 
-int izq_1=22;
-int izq_2=24;
-int centro_3=26;
-int der_4=28;
-int der_5=30;
+int izq_1=52;
+int izq_2=48;
+int centro_3=51;
+int der_4=50;
+int der_5=53;
 
-int l1=1;
-int l2=1;
-int l3=1;
-int l4=1;
-int l5=1;
+int l1=0;
+int l2=0;
+int l3=0;
+int l4=0;
+int l5=0;
 
 //colores
 int neg=0;
@@ -45,6 +45,22 @@ void loop() {
   if(l1==1 && l2==1 && l4==1 && l5==1){
     Avanzar();
   }
+/*Posibles intersecciones
+  if(l2==0 && l3==0 && l4==0){
+    Avanzar();
+  }
+  if (l1==0 && l2==0 && l3==0 && l4==0 && l5==0){
+    Avanzar();    
+  }
+
+  if(l2==0 && l4==0){
+    Avanzar();
+  }*/
+
+  if(l1==0 && l2==0 || l4==0 && l5==0 || l2==0  && l4==0){
+    Recuperar();
+  }
+  
 //Muy desviado a la izquierda
   if(l1==0 || l1==0 && l2==0 || l1==0 && l2==0 && l4==0){
     GirarI2();
@@ -61,18 +77,6 @@ void loop() {
   if(l4==0){
     GirarD1();
   }  
-//Posibles intersecciones
-  if(l2==0 && l3==0 && l4==0){
-    Avanzar();
-  }
-  if (l1==0 && l2==0 && l3==0 && l4==0 && l5==0){
-    Avanzar();
-    
-  }
-
-  if(l2==0 && l4==0){
-    Avanzar();
-  }
   
 }
 
@@ -92,29 +96,41 @@ void GirarI2(){
 }
 
 void GirarD1(){
-  Md.setSpeed(100);
+  Md.setSpeed(200);
   Md.run(BACKWARD);
   Mi.setSpeed(225);
   Mi.run(FORWARD);
 }
 
 void GirarI1(){
-  Mi.setSpeed(100);
+  Mi.setSpeed(200);
   Mi.run(BACKWARD);
   Md.setSpeed(225);
   Md.run(FORWARD);
 }
 
 void Avanzar(){
-  Md.setSpeed(200);
+  Md.setSpeed(250);
   Md.run(FORWARD);
-  Mi.setSpeed(200);
+  Mi.setSpeed(250);
   Mi.run(FORWARD);
 }
 void Retroceder(){
-  Md.setSpeed(200);
+  Md.setSpeed(250);
   Md.run(FORWARD);
-  Mi.setSpeed(200);
+  Mi.setSpeed(250);
   Mi.run(FORWARD);
+}
+
+
+void Recuperar(){
+  delay(1000);
+  Retroceder();
+  delay(1000);
+  Md.setSpeed(250);
+  Md.run(FORWARD);
+  Mi.setSpeed(0);
+  Mi.run(RELEASE);
+  delay(500);
 }
 
