8cf983fed8b7fc3cd8542aaf7488c30f852b6cce
monse1234 <33985586+monse1234@users.noreply.github.com>
2018-02-02 19:55:15 -0600

    Add files via upload

---
+++ Prueba_inclinacion.ino
@@ -0,0 +1,24 @@
+int SI=53;
+int inclina=1;
+
+void setup() {
+ Serial.begin(9600);
+ pinMode(SI,INPUT);
+
+}
+
+void loop() {
+inclina=digitalRead(SI);
+
+Serial.println(inclina);  
+
+if(inclina==0){
+  Serial.println("Inclinado");  
+}
+else{
+  Serial.println("Horizontal");  
+}
+
+delay(2000);
+
+}
