0138bdd5a8646a93d63629c14cbe2cfa26530228
israellh2001 <Israellh2001@hotmail.com>
2018-02-17 13:44:32 -0600

    Cambios en la libreria

--- maze/Sensor Temp/Adafruit_MLX90614_Library:I2C/Adafruit_MLX90614.cpp
+++ maze/Sensor Temp/Adafruit_MLX90614_Library:I2C/Adafruit_MLX90614.cpp
@@ -1,4 +1,4 @@
-/*************************************************** 
+/***************************************************
   This is a library for the MLX90614 Temp Sensor
 
   Designed specifically to work with the MLX90614 sensors in the
@@ -6,25 +6,25 @@
   ----> https://www.adafruit.com/products/1748
   ----> https://www.adafruit.com/products/1749
 
-  These sensors use I2C to communicate, 2 pins are required to  
+  These sensors use I2C to communicate, 2 pins are required to
   interface
-  Adafruit invests time and resources providing this open source code, 
-  please support Adafruit and open-source hardware by purchasing 
+  Adafruit invests time and resources providing this open source code,
+  please support Adafruit and open-source hardware by purchasing
   products from Adafruit!
 
-  Written by Limor Fried/Ladyada for Adafruit Industries.  
+  Written by Limor Fried/Ladyada for Adafruit Industries.
   BSD license, all text above must be included in any redistribution
  ****************************************************/
 
 #include "Adafruit_MLX90614.h"
 
-Adafruit_MLX90614::Adafruit_MLX90614(uint8_t i2caddr) {
+Adafruit_MLX90614::Adafruit_MLX90614_Patos(uint8_t i2caddr) {
   _addr = i2caddr;
 }
 
 
-boolean Adafruit_MLX90614::begin(void) {
-  Wire.begin();
+boolean Adafruit_MLX90614_Patos::begin(void) {
+  myWire.begin();
 
   /*
   for (uint8_t i=0; i<0x20; i++) {
@@ -38,42 +38,51 @@ boolean Adafruit_MLX90614::begin(void) {
 //////////////////////////////////////////////////////
 
 
-double Adafruit_MLX90614::readObjectTempF(void) {
+double Adafruit_MLX90614_Patos::readObjectTempF(void) {
   return (readTemp(MLX90614_TOBJ1) * 9 / 5) + 32;
 }
 
 
-double Adafruit_MLX90614::readAmbientTempF(void) {
+double Adafruit_MLX90614_Patos::readAmbientTempF(void) {
   return (readTemp(MLX90614_TA) * 9 / 5) + 32;
 }
 
-double Adafruit_MLX90614::readObjectTempC(void) {
+double Adafruit_MLX90614_Patos::readObjectTempC(void) {
   return readTemp(MLX90614_TOBJ1);
 }
 
 
-double Adafruit_MLX90614::readAmbientTempC(void) {
+double Adafruit_MLX90614_Patos::readAmbientTempC(void) {
   return readTemp(MLX90614_TA);
 }
 
-float Adafruit_MLX90614::readTemp(uint8_t reg) {
+float Adafruit_MLX90614_Patos::readTemp(uint8_t reg) {
   float temp;
-  
+
   temp = read16(reg);
   temp *= .02;
   temp  -= 273.15;
   return temp;
 }
+/*********************************************************************/
+int Adafruit_MLX90614_Patos::setSda_Scl(int x,int y){
+  Adafruit_MLX90614_Patos::sda=x;
+  Adafruit_MLX90614_Patos::scl=y;
+}
 
+boolean Adafruit_MLX90614_Patos::init(void){
+  SoftwareWire myWire(Adafruit_MLX90614_Patos::sda,Adafruit_MLX90614_Patos::scl);
+  return true;
+}
 /*********************************************************************/
 
-uint16_t Adafruit_MLX90614::read16(uint8_t a) {
+uint16_t Adafruit_MLX90614_Patos::read16(uint8_t a) {
   uint16_t ret;
 
-  Wire.beginTransmission(_addr); // start transmission to device 
+  Wire.beginTransmission(_addr); // start transmission to device
   Wire.write(a); // sends register address to read from
   Wire.endTransmission(false); // end transmission
-  
+
   Wire.requestFrom(_addr, (uint8_t)3);// send data n-bytes read
   ret = Wire.read(); // receive DATA
   ret |= Wire.read() << 8; // receive DATA
--- maze/Sensor Temp/Adafruit_MLX90614_Library:I2C/Adafruit_MLX90614.h
+++ maze/Sensor Temp/Adafruit_MLX90614_Library:I2C/Adafruit_MLX90614.h
@@ -1,4 +1,4 @@
-/*************************************************** 
+/***************************************************
   This is a library for the MLX90614 Temp Sensor
 
   Designed specifically to work with the MLX90614 sensors in the
@@ -6,10 +6,10 @@
   ----> https://www.adafruit.com/products/1748
   ----> https://www.adafruit.com/products/1749
 
-  These sensors use I2C to communicate, 2 pins are required to  
+  These sensors use I2C to communicate, 2 pins are required to
   interface
-  Adafruit invests time and resources providing this open source code, 
-  please support Adafruit and open-source hardware by purchasing 
+  Adafruit invests time and resources providing this open source code,
+  please support Adafruit and open-source hardware by purchasing
   products from Adafruit!
 
   Written by Limor Fried/Ladyada for Adafruied in any redistribution
@@ -21,7 +21,7 @@
 #else
  #include "WProgram.h"
 #endif
-#include "Wire.h"
+#include "SoftwareWire.h"
 
 
 #define MLX90614_I2CADDR 0x5A
@@ -46,12 +46,14 @@
 #define MLX90614_ID4 0x3F
 
 
-class Adafruit_MLX90614  {
+class Adafruit_MLX90614_Patos  {
  public:
   Adafruit_MLX90614(uint8_t addr = MLX90614_I2CADDR);
   boolean begin();
   uint32_t readID(void);
-
+  int sda,scl;
+  boolean init();
+  int setSda_Scl(int x,int y);
   double readObjectTempC(void);
   double readAmbientTempC(void);
   double readObjectTempF(void);
@@ -64,4 +66,3 @@ class Adafruit_MLX90614  {
   uint16_t read16(uint8_t addr);
   void write16(uint8_t addr, uint16_t data);
 };
-
