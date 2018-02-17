2458d0a75a8a14afd698399b36525f5a4a4650ab
CharsMtz <charley201050@hotmail.com>
2018-02-17 11:21:31 -0600

    Seguidor línea con RGB
    - Pista completa
    - RGB (calibrar)
    - Topes
    - Subidas

--- Seguidor de Línea/Seguidor_de_linea/Seguidor_de_linea.ino
+++ Seguidor de Línea/Seguidor_de_linea/Seguidor_de_linea.ino
@@ -1,13 +1,47 @@
 #include <ArduinoMotorShieldR3.h>   //Libreria para usar el motor shield de arduino
+#include <Wire.h>           //Libreria para usar la comuniación I2C
+#include <SFE_ISL29125.h>   //Libreria para usar el sensor RGB
+#include <SoftwareWire.h>   //Libreria para usar I2C en otros pines digitales
 
 ArduinoMotorShieldR3 motores;   //Declaración de los motores
 //M1 = motor derecho
 //M2 = motor izquierdo
 
+SFE_ISL29125 RGB_sensor;    //Creación del 1° Sensor RGB
+SoftwareWire myWire( 4, 5);   //Configurar pines 4(SDA) y 5(SCL) con I2C (Puede ser cualquier pin digital)
+
+//Direcciones del 2° sensor
+#define _addr 0x44                  //Dirección del sensor
+#define DEVICE_ID 0x00              //ID del sensor, verifica si el sensor funciona correctamente
+#define CFG1_MODE_RGB 0x05          //Configuración del sensor para que lea rojo, verde y azul
+#define CFG1_10KLUX 0x08            //Configuracion del rango de luz de lectura en 10,000
+#define CFG2_IR_ADJUST_HIGH 0x3F    //Configura el filtro de infrarrojo en high
+#define CFG_DEFAULT 0x00            //Configuración estándar de lectura
+
+//Para configurar el sensor se usa config2 y necesita 3 parámetros
+#define CONFIG_1 0x01               //En él se guardan el primer parámetro de configuración
+#define CONFIG_2 0x02               //En él se guardan el segundo parámetro de configuración
+#define CONFIG_3 0x03               //En él se guardan el tercer parámetro de configuración
+
+#define STATUS 0x08                 //En él guarda el estado del sensor
+
+//Variables de la pista
 #define blanco 1
 #define negro 0
-#define velI 160
-#define velD 163
+//Velocidades de los motores
+#define velI 150    //160
+#define velD 153    //163
+
+//Variables de ambos sensores RGB
+//1° sensor RGB
+uint16_t r1=0;
+uint16_t v1=0;
+uint16_t a1=0;
+
+//2° sensor RGB
+uint16_t r2=0;
+uint16_t v2=0;
+uint16_t a2=0;
 
 //SENSOR INFRARROJO
 //Pines del sensor infrarrojo
@@ -40,8 +74,8 @@ int dl7=0;
 int dl8=0;
 
 //valores de blanco y negro
-int bco=620;  //600
-int neg=720;
+int bco=620;  //620
+int neg=720;  //720
 
 void setup(){
   //Se inicia la comunicación Serial
@@ -49,6 +83,16 @@ void setup(){
 
   //Iniciar motores
   motores.init();
+
+  //Iniciar sensores RGB
+  if (RGB_sensor.init())    //Iniciar 1° sensor RGB
+  {
+    Serial.println("Sensor 1 iniciado");
+  }
+
+  if(init2()){              //Iniciar 2° sensor RGB
+    Serial.println("Sensor 2 iniciado");
+  }
 
   //Configurando pines del sensor infrarrojo como entradas
   pinMode(izq_1,INPUT);
@@ -80,6 +124,7 @@ void loop() {
   if (l6<=bco){dl6=1;} else {dl6=0;}
   if (l7<=bco){dl7=1;} else {dl7=0;}
   if (l8<=bco){dl8=1;} else {dl8=0;}
+  
   //Moverse en linea recta
   Moverse(velD,velI);
 
@@ -115,12 +160,10 @@ void loop() {
   //Curvas de 90°
   if(dl8==negro && dl7==negro && dl6==negro || dl8==negro && dl7==negro && dl6==negro && dl5==negro){
     Moverse(-130,200);
-    //Moverse(-250,-250);
   }
 
   if(dl1==negro && dl2==negro && dl3==negro || dl1==negro && dl2==negro && dl3==negro && dl4==negro){
     Moverse(200,-130);
-    //Moverse(-250,-250);
   }
 
   //Intersección
@@ -142,13 +185,154 @@ int Moverse(int velD_, int velI_){
 
 //Función para imprimir valores del sensor
 void imprimirDatosInf(){
-  Serial.print("Izq 1: ");Serial.print(l1);Serial.print("\ D Izq 1: ");Serial.println(dl1);
-  Serial.print("Izq 2: ");Serial.print(l2);Serial.print("\ D Izq 1: ");Serial.println(dl2);
-  Serial.print("Izq 3: ");Serial.print(l3);Serial.print("\ D Izq 1: ");Serial.println(dl3);
-  Serial.print("Cen i: ");Serial.print(l4);Serial.print("\ D Izq 1: ");Serial.println(dl4);
-  Serial.print("Cen d: ");Serial.print(l5);Serial.print("\ D Izq 1: ");Serial.println(dl5);
-  Serial.print("Der 6: ");Serial.print(l6);Serial.print("\ D Izq 1: ");Serial.println(dl6);
-  Serial.print("Der 7: ");Serial.print(l7);Serial.print("\ D Izq 1: ");Serial.println(dl7);
-  Serial.print("Der 8: ");Serial.print(l8);Serial.print("\ D Izq 1: ");Serial.println(dl8);
+  Serial.print("Izq 1: ");Serial.print(l1);Serial.print("\t / D Izq 1: ");Serial.println(dl1);
+  Serial.print("Izq 2: ");Serial.print(l2);Serial.print("\t / D Izq 1: ");Serial.println(dl2);
+  Serial.print("Izq 3: ");Serial.print(l3);Serial.print("\t / D Izq 1: ");Serial.println(dl3);
+  Serial.print("Cen i: ");Serial.print(l4);Serial.print("\t / D Izq 1: ");Serial.println(dl4);
+  Serial.print("Cen d: ");Serial.print(l5);Serial.print("\t / D Izq 1: ");Serial.println(dl5);
+  Serial.print("Der 6: ");Serial.print(l6);Serial.print("\t / D Izq 1: ");Serial.println(dl6);
+  Serial.print("Der 7: ");Serial.print(l7);Serial.print("\t / D Izq 1: ");Serial.println(dl7);
+  Serial.print("Der 8: ");Serial.print(l8);Serial.print("\t / D Izq 1: ");Serial.println(dl8);
   Serial.println("-----------------------------------");
 }
+
+void leerRGB(){
+  //Valores del 1° sensor RGB
+  r1 = RGB_sensor.readRed();
+  v1 = RGB_sensor.readGreen();
+  a1 = RGB_sensor.readBlue();
+
+  //Valores del 2° sensor RGB
+  r2 = read16(0x0B);
+  v2 = read16(0x09);
+  a2 = read16(0x0D);
+}
+
+void imprimirRGB(){
+  //Imprimir valores del 1° RGB (en decimal)
+  Serial.print("Rojo 1: "); Serial.println(r1,DEC);
+  Serial.print("Verde 1: "); Serial.println(v1,DEC);
+  Serial.print("Azul 1: "); Serial.println(a1,DEC);
+  Serial.println();
+
+  //Imprimir valores del 2° RGB
+  Serial.print("Rojo 2: ");Serial.println(r2);
+  Serial.print("Verde 2: ");Serial.println(v2);
+  Serial.print("Azul 2: ");Serial.println(a2);
+  Serial.println("-----------------------------");
+}
+
+//Funciones para configurar y leer el 2° RGB///////////////////////
+uint16_t read16(uint8_t reg)
+{
+  uint16_t data = 0x0000;
+
+  myWire.beginTransmission(_addr);
+  myWire.write(reg);
+  myWire.endTransmission();
+  
+  myWire.beginTransmission(_addr);
+  myWire.requestFrom(_addr, (uint8_t)2); // request 2 bytes of data
+  data = myWire.read();
+  data |= (myWire.read() << 8);
+  myWire.endTransmission();
+
+  return data;
+}
+
+bool init2()
+{
+  bool ret = true;
+  uint8_t data = 0x00;
+  
+  // Start I2C
+  myWire.begin();
+  
+  // Check device ID
+  data = read8(DEVICE_ID);
+  if (data != 0x7D)
+  {
+    ret &= false;
+  }
+  
+  // Reset registers
+  ret &= reset();
+  
+  // Set to RGB mode, 10k lux, and high IR compensation
+  ret &= config2(CFG1_MODE_RGB | CFG1_10KLUX, CFG2_IR_ADJUST_HIGH, CFG_DEFAULT);
+  
+  return ret;
+}
+
+bool config2(uint8_t config1, uint8_t config2, uint8_t config3)
+{
+  bool ret = true;
+  uint8_t data = 0x00;
+  
+  // Set 1st configuration register
+  write8(CONFIG_1, config1);
+  // Set 2nd configuration register
+  write8(CONFIG_2, config2);
+  // Set 3rd configuration register
+  write8(CONFIG_3, config3);
+  
+  // Check if configurations were set correctly
+  data = read8(CONFIG_1);
+  if (data != config1)
+  {
+    ret &= false;
+  }
+  data = read8(CONFIG_2);
+  if (data != config2)
+  {
+    ret &= false;
+  }
+  data = read8(CONFIG_3);
+  if (data != config3)
+  {
+    ret &= false;
+  }
+  return ret;
+}
+
+uint8_t read8(uint8_t reg)
+{
+  myWire.beginTransmission(_addr);
+  myWire.write(reg);
+  myWire.endTransmission();
+  myWire.beginTransmission(_addr);
+  myWire.requestFrom(_addr,(uint8_t)1);
+  uint8_t data = myWire.read();
+  myWire.endTransmission();
+  
+  return data;
+}
+
+bool reset()
+{
+  uint8_t data = 0x00;
+  // Reset registers
+  write8(DEVICE_ID, 0x46);
+  // Check reset
+  data = read8(CONFIG_1);
+  data |= read8(CONFIG_2);
+  data |= read8(CONFIG_3);
+  data |= read8(STATUS);
+  if (data != 0x00)
+  {
+    return false;
+  }
+  return true;
+}
+
+void write8(uint8_t reg, uint8_t data)
+{
+  myWire.beginTransmission(_addr);
+  myWire.write(reg);
+  myWire.write(data);
+  myWire.endTransmission();
+  
+  return;
+}
+
+
