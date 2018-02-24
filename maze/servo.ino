22b6cb0f6b1a6fb6213a936ffd91fd483816381c
Martin Contreras <conromo64@gmail.com>
2018-02-24 10:29:00 -0600

    Add files via upload

---
+++ maze/servo.ino
@@ -0,0 +1,19 @@
+#include <AFMotor.h>
+#include <Servo.h>
+Servo kits;
+
+int servo_position=0;
+
+
+void setup() {
+  // put your setup code here, to run once:
+  kits.attach(9);
+}
+
+void loop() {
+  kits.write(0);
+  delay(8000);
+  kits.write(360);
+  delay(1000);
+  
+}
