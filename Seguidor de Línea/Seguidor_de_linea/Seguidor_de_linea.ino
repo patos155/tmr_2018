54daadb5595a6f08200215b14b578eeefc09effa
CharsMtz <charley201050@hotmail.com>
2018-03-10 09:57:52 -0600

    Add files via upload

--- Seguidor de Línea/Seguidor_de_linea/Seguidor_de_linea.ino
+++ Seguidor de Línea/Seguidor_de_linea/Seguidor_de_linea.ino
@@ -1,19 +1,60 @@
-#include <ArduinoMotorShieldR3.h>   //Libreria para usar el nuevo motor shield
+#include <ArduinoMotorShieldR3.h>   //Libreria para usar el motor shield de arduino
+#include <Wire.h>           //Libreria para usar la comuniación I2C
+#include <SFE_ISL29125.h>   //Libreria para usar el sensor RGB
+#include <SoftwareWire.h>   //Libreria para usar I2C en otros pines digitales
 
 ArduinoMotorShieldR3 motores;   //Declaración de los motores
+//M1 = motor derecho
+//M2 = motor izquierdo
+
+SFE_ISL29125 RGB_sensor;    //Creación del 1° Sensor RGB
+SoftwareWire myWire( 4, 5);   //Configurar pines 4(SDA) y 5(SCL) con I2C (Puede ser cualquier pin digital)
+
+//Direcciones del 2° sensor
+#define _addr 0x44                  //Dirección del sensor
+#define DEVICE_ID 0x00              //ID del sensor, verifica si el sensor funciona correctamente
+#define CFG1_MODE_RGB 0x05          //Configuración del sensor para que lea rojo, verde y azul
+#define CFG1_10KLUX 0x08            //Configuracion del rango de luz de lectura en 10,000
+#define CFG2_IR_ADJUST_HIGH 0x3F    //Configura el filtro de infrarrojo en high
+#define CFG_DEFAULT 0x00            //Configuración estándar de lectura
+
+//Para configurar el sensor se usa config2 y necesita 3 parámetros
+#define CONFIG_1 0x01               //En él se guardan el primer parámetro de configuración
+#define CONFIG_2 0x02               //En él se guardan el segundo parámetro de configuración
+#define CONFIG_3 0x03               //En él se guardan el tercer parámetro de configuración
+
+#define STATUS 0x08                 //En él guarda el estado del sensor
+
+//Variables de la pista
+#define blanco 1
+#define negro 0
+//Velocidades de los motores
+int velI=140;    //160
+int velD=140;    //163
+
+//Variables de ambos sensores RGB
+//1° sensor RGB
+uint16_t r1=0;
+uint16_t v1=0;
+uint16_t a1=0;
+
+//2° sensor RGB
+uint16_t r2=0;
+uint16_t v2=0;
+uint16_t a2=0;
 
 //SENSOR INFRARROJO
 //Pines del sensor infrarrojo
-int izq_1=A5;
-int izq_2=A8;
-int izq_3=A9;
-int centro_i=A10;
-int centro_d=A11;
-int der_6=A12;
-int der_7=A13;
-int der_8=A14;
+int izq_1=A8;
+int izq_2=A9;
+int izq_3=A10;
+int centro_i=A11;
+int centro_d=A12;
+int der_6=A13;
+int der_7=A14;
+int der_8=A15;
 
-//Variables donde se guardan las lecuturas del sensor infrarrojo 
+//Variables donde se guardan las lecuturas del sensor infrarrojo
 int l1=0;
 int l2=0;
 int l3=0;
@@ -23,17 +64,26 @@ int l6=0;
 int l7=0;
 int l8=0;
 
+int dl1=0;
+int dl2=0;
+int dl3=0;
+int dl4=0;
+int dl5=0;
+int dl6=0;
+int dl7=0;
+int dl8=0;
+
 //valores de blanco y negro
-int bco=680;
-int neg=720;
+int bco=620;  //620
+int neg=720;  //720
 
-//SENSOR ULTRASÓNICO
-//Pines
-int Trig=52;
-int Echo=53;
+//Sensor ultrasónico
+const int EchoPin = 53;
+const int TriggerPin = 52;
 
-//Valores
-int dist=0;
+//Sensor de inclinación
+int s_inc=50;
+int inc=0;
 
 void setup(){
   //Se inicia la comunicación Serial
@@ -41,6 +91,16 @@ void setup(){
 
   //Iniciar motores
   motores.init();
+
+  //Iniciar sensores RGB
+  if (RGB_sensor.init())    //Iniciar 1° sensor RGB
+  {
+    Serial.println("Sensor 1 iniciado");
+  }
+
+  if(init2()){              //Iniciar 2° sensor RGB
+    Serial.println("Sensor 2 iniciado");
+  }
 
   //Configurando pines del sensor infrarrojo como entradas
   pinMode(izq_1,INPUT);
@@ -52,14 +112,20 @@ void setup(){
   pinMode(der_7,INPUT);
   pinMode(der_8,INPUT);
 
-  //Configuración del sensor ultrasónico
-  pinMode(Echo,INPUT);
-  pinMode(Trig,OUTPUT);
+  //Inclinación
+  pinMode(s_inc,INPUT);
+
+  //Ultrasónico
+  pinMode(TriggerPin, OUTPUT);
+  pinMode(EchoPin, INPUT);
 }
 
-void loop(){
-
-//Se guardan datos del sensor infrarrojo
+void loop() {
+  //Inclinación
+  inc=digitalRead(s_inc);
+  //Ultrasónico
+  int cm = ping(TriggerPin, EchoPin);
+  //Se guardan datos del sensor infrarrojo
   l1=analogRead(izq_1);
   l2=analogRead(izq_2);
   l3=analogRead(izq_3);
@@ -68,194 +134,300 @@ void loop(){
   l6=analogRead(der_6);
   l7=analogRead(der_7);
   l8=analogRead(der_8);
+  //Conversion de datos a 0 1  1=blanco 0=negro
+  if (l1<=bco){dl1=1;} else {dl1=0;}
+  if (l2<=bco){dl2=1;} else {dl2=0;}
+  if (l3<=bco){dl3=1;} else {dl3=0;}
+  if (l4<=bco){dl4=1;} else {dl4=0;}
+  if (l5<=bco){dl5=1;} else {dl5=0;}
+  if (l6<=bco){dl6=1;} else {dl6=0;}
+  if (l7<=bco){dl7=1;} else {dl7=0;}
+  if (l8<=bco){dl8=1;} else {dl8=0;}
 
-//Lectura del sensor ultrasónico
-  dist=ping(Trig,Echo);
+  if (cm<10);{
+    ///RETROCEDER
+    motores.setM1Speed (-90);
+    motores. setM2Speed (-90);
+    delay (1000);
+    //Quieto
+    motores.setM1Speed (0);
+    motores.setM2Speed (0);
+    delay (500);
+    //GIRAR A LA DERECHA
+    motores.setM1Speed (-150);
+    motores.setM2Speed (190);
+    delay (500);
+     //AVANZAR
+    motores.setM1Speed (100);
+    motores.setM2Speed (100);
+    delay (2500);
+    //GIRAR A LA IZQUIERDA
+    motores.setM1Speed (180);
+    motores.setM2Speed (-160);
+    delay (500);
+    //AVANZAR
+    motores.setM1Speed (100);
+    motores.setM2Speed (100);
+    delay (3100);
+    //GIRAR A LA IZQUIERDA
+    motores.setM1Speed (150);
+    motores.setM2Speed (-190);
+    delay (500);
+    //AVANZAR
+    motores.setM1Speed (100);
+    motores.setM2Speed (100);
+    delay (1000);
+   //SE INCORPORA A LA LINEA
+    motores.setM2Speed (100);
+    motores.setM2Speed (100);
+    delay (500);
+    //Quieto
+    motores.setM1Speed (0);
+    motores.setM2Speed (0);
+    delay (500);
+    }
+  
+  //Moverse en linea recta
+  Moverse(velD,velI);
 
-  //leerRGB();
-  //imprimirDatosInf();
-  //imprimirDatosRGB();
-  //delay(1000);
-
-//Encontrar objeto
-  if(dist<=7 && dist>0){
-    Quieto();
-    delay(1000);
+  //Inclinación
+  if(inc==0){
+    velD=350;
+    velI=350; 
+  }
+  if(inc==1){
+    velD=140;
+    velI=140;
   }
 
-//Línea cortada
-  if(l1<=bco && l2<=bco && l3<=bco && l4<=bco && l5<=bco && l6<=bco && l7<=bco && l8<=bco || l4>=neg && l5>=neg){
-    Avanzar();
-  }
-
-  else if(l4>=neg && l5>=neg && (l3>=neg || l6>=neg)){
-    Avanzar();
+  if(dl3==negro && dl6==negro){
+    Moverse(velD,velI);
   }
 
   //Muy desviado a la izquierda
-  else if(l1>=neg || l1>=neg && l2>=neg || l1>=neg && l2>=neg && l3>=neg || l1>=neg && l2>=neg && l3>=neg && l4>=neg || l1>=neg && l2>=neg && l3>=neg && l4>=neg && l5>=neg || l1>=neg && l2>=neg && l3>=neg && l4>=neg && l5>=neg && l6>=neg || l1>=neg && l2>=neg && l3>=neg && l4>=neg && l5>=neg && l6>=neg && l7>=neg){    //Curvas cerradas
-    GirarI2();            //Gira a la izquierda con mayor velocidad normal
-  }
-//Muy desviado a la derecha
-  else if(l8>=neg || l8>=neg && l7>=neg || l8>=neg && l7>=neg && l6>=neg || l8>=neg && l7>=neg && l6>=neg && l5>=neg || l8>=neg && l7>=neg && l6>=neg && l5>=neg && l4>=neg || l8>=neg && l7>=neg && l6>=neg && l5>=neg && l4>=neg && l3>=neg || l8>=neg && l7>=neg && l6>=neg && l5>=neg && l4>=neg && l3>=neg && l2>=neg){    //Curvas cerradas
-    GirarD2();            //Gira a la derecha con mayor velocidad normal 
+  if(dl8==negro && dl7==negro && dl3==negro){
+    Moverse(-400,350);//-250,200
   }
 
-/*/Desviado un poco a la izquierda
-  else if(l4>=neg && l5<=bco || l3>=neg && l5<=bco || l2>=neg && l5<=bco){
-    GirarI1();            //Gira a la izquierda con velocidad normal
+  //Muy desviado a la derecha
+  if(dl1==negro && dl2==negro && dl6==negro){
+    Moverse(350,-400);//200,-250
   }
-//Desviado un poco a la derecha
-  else if(l5>=neg && l4<=bco || l6>=neg && l4<=bco || l7>=neg && l4<=bco){
-    GirarD1();            //Gira a la derecha con velocidad normal 
+
+  //Centrado
+  if(dl8==blanco && dl7==blanco && dl2==blanco && dl1==blanco){
+    Moverse(velD,velI);
+  }
+
+  //Ligeramente desviado a la izquierda
+  if(dl2==negro && dl8==blanco && dl1==blanco){
+    Moverse(280,-200);//220,-140
+  }
+
+  //Ligeramente desviado a la derecha
+  if(dl7==negro && dl1==blanco && dl5==blanco){
+    Moverse(-200,280);//-120,200
+  }
+
+  //Curvas de 90°
+  if(dl8==negro && dl7==negro && dl6==negro || dl8==negro && dl7==negro && dl6==negro && dl5==negro){
+    Moverse(-130,200);
+  }
+
+  if(dl1==negro && dl2==negro && dl3==negro || dl1==negro && dl2==negro && dl3==negro && dl4==negro){
+    Moverse(200,-130);
+  }
+
+  if(dl2==negro || dl2==negro && dl1==negro){
+    Moverse(200,-250);
+  }
+  if(dl7==negro || dl7==negro && dl8==negro){
+    Moverse(-250,200);  
+  }
+
+  /*//Intersección
+  if(dl1==negro && dl8==negro){
+    Moverse(0,0);
+    delay(1000);
   }*/
 
-  //Desviado un poco a la izquierda
-  else if(l2>=neg && l3>=neg){
-    GirarI1();            //Gira a la izquierda con velocidad normal
-  }
-//Desviado un poco a la derecha
-  else if(l6>=neg && l7>=neg){
-    GirarD1();            //Gira a la derecha con velocidad normal 
-  }
-
-  /*else if(l3>=neg && l4>=neg && l5<=bco || l2>=neg && l3>=neg){
-    GirarI3();
-  }
-
-  else if(l6>=neg && l5>=neg && l4<=bco || l7>=neg && l6>=neg){
-    GirarD3();
-  }*/
-
-/*90° Izquierda
-  if(l1==0 && 12==0 || l1==0 && l2==0 && l3==0 || l1==0 && l2==0 && l3==0 && l4==0){
-    GirarI2();            //Gira a la izquierda con mayor velocidad
-  }
-//90° Derecha
-  if(l5==0 && l4==0 || l5==0 && l4==0 && l3==0 || l5==0 && l4==0 && l3==0 && l2==0){
-    GirarD2();            //Gira a la derecha con mayor velocidad
-  }
-*/
-
-//Interseccion
-  else if(l1>=neg && l8>=neg && l2>=neg && l7>=neg){     //Si se encuentra una intersección
-    Quieto();
-    /*delay(1000);
-    leerRGB();                                  //Lee los colores que los sensores detectan
-    //imprimirRGB();
-    //delay(800);
-    if(v1<480 && v1>450 && v1<r1 && v1<a1 && v2<700 && v2>544 && v2<r2 && v2<a2){     //Si ambos sensores detectan verde
-      Retorno();                                            //Da media vuleta y vuelve a seguir la línea
-      delay(1000);
-    }else{
-      if(v1<480 && v1>450 && v1<r1 && v1<a1 && v2>544){                        //Si verde es mayor a 1000 y menor a rojo y azul
-      GirarI2();
-      delay(250);                                          //Da vuelta hacia la izquierda
-      }else{
-        if(v2<700 && v2>544 && v2<r2 && v2<a2 && v1>480){                      //Si verde es mayor a 1000 y menor a rojo y azul
-          GirarD2();                                          //Da vuelta hacia la derecha
-          delay(250);
-        }else{                                              //Si no detecta ningun verde
-          Avanzar();
-        }
-      }
-    }
-    Quieto();
-    delay(1000);*/
-  }
+  //imprimirDatosInf();
+  //delay(800);
   
+
+}
+//Movimiento de los motores
+int Moverse(int velD_, int velI_){
+  motores.setM1Speed(velD_);
+  motores.setM2Speed(velI_);
 }
 
-//Velocidad máxima de motores: 400
-//Función para girar a la derecha con velocidad normal
-void GirarD3(){
-  //Serial.println("GirarD1");
-  motores.setM1Speed(-20); //-20
-  motores.setM2Speed(80);  //80
-}
-
-//Función para girar a la izquierda con velocidad normal
-void GirarI3(){
-  //Serial.println("GirarI1");
-  motores.setM2Speed(-20);  //-20
-  motores.setM1Speed(80);  //80
-}
-
-//Función para girar a la derecha con mayor velocidad
-void GirarD2(){
-  //Serial.println("GirarD2");
-  motores.setM1Speed(-255); //170
-  motores.setM2Speed(200);  //200
-}
-
-//Función para girar a la izquierda con mayor velocidad
-void GirarI2(){
-  //Serial.println("GirarI2");
-  motores.setM2Speed(-255); //-170
-  motores.setM1Speed(200);  //200
-}
-
-//Función para girar a la derecha con velocidad normal
-void GirarD1(){
-  //Serial.println("GirarD1");
-  motores.setM1Speed(-120); //-130
-  motores.setM2Speed(200);  //180
-}
-
-//Función para girar a la izquierda con velocidad normal
-void GirarI1(){
-  //Serial.println("GirarI1");
-  motores.setM2Speed(-20);  //-130
-  motores.setM1Speed(200);  //180
-}
-
-//Función para avanzar 
-void Avanzar(){
-  //Serial.println("Avanza");
-  motores.setM1Speed(125);  //125
-  motores.setM2Speed(100);  //100
-}
-
-//Funcióon para girar sobre su eje
-void Retorno(){
-  //Serial.println("Retorno");
-  motores.setM2Speed(100);
-  motores.setM1Speed(-100);
-}
-
-//Función para detenerse
-void Quieto(){
-  //Serial.println("Quieto");
-  motores.setM1Speed(0);
-  motores.setM2Speed(0);
+//Calcular distancia del ultrasónico
+int ping(int TriggerPin, int EchoPin) {
+   long duration, distanceCm;
+   
+   digitalWrite(TriggerPin, LOW);  //para generar un pulso limpio ponemos a LOW 4us
+   delayMicroseconds(4);
+   digitalWrite(TriggerPin, HIGH);  //generamos Trigger (disparo) de 10us
+   delayMicroseconds(10);
+   digitalWrite(TriggerPin, LOW);
+   
+   duration = pulseIn(EchoPin, HIGH);  //medimos el tiempo entre pulsos, en microsegundos
+   
+   distanceCm = duration * 10 / 292/ 2;   //convertimos a distancia, en cm
+   return distanceCm;
 }
 
 //Función para imprimir valores del sensor
 void imprimirDatosInf(){
-  Serial.print("Izq 1:");Serial.println(l1);
-  Serial.print("Izq 2:");Serial.println(l2);
-  Serial.print("Izq 3:");Serial.println(l3);
-  Serial.print("Centro i:");Serial.println(l4);
-  Serial.print("Centro d:");Serial.println(l5);
-  Serial.print("Der 6:");Serial.println(l6);
-  Serial.print("Der 7:");Serial.println(l7);
-  Serial.print("Der 8:");Serial.println(l8);
+  Serial.print("Izq 1: ");Serial.print(l1);Serial.print("\t / D Izq 1: ");Serial.println(dl1);
+  Serial.print("Izq 2: ");Serial.print(l2);Serial.print("\t / D Izq 1: ");Serial.println(dl2);
+  Serial.print("Izq 3: ");Serial.print(l3);Serial.print("\t / D Izq 1: ");Serial.println(dl3);
+  Serial.print("Cen i: ");Serial.print(l4);Serial.print("\t / D Izq 1: ");Serial.println(dl4);
+  Serial.print("Cen d: ");Serial.print(l5);Serial.print("\t / D Izq 1: ");Serial.println(dl5);
+  Serial.print("Der 6: ");Serial.print(l6);Serial.print("\t / D Izq 1: ");Serial.println(dl6);
+  Serial.print("Der 7: ");Serial.print(l7);Serial.print("\t / D Izq 1: ");Serial.println(dl7);
+  Serial.print("Der 8: ");Serial.print(l8);Serial.print("\t / D Izq 1: ");Serial.println(dl8);
   Serial.println("-----------------------------------");
 }
 
-//Conversión para el sensor ultrasónico
-int ping(int TriggerPin, int EchoPin){
-  long duration, distanceCm;
+void leerRGB(){
+  //Valores del 1° sensor RGB
+  r1 = RGB_sensor.readRed();
+  v1 = RGB_sensor.readGreen();
+  a1 = RGB_sensor.readBlue();
 
-  digitalWrite(TriggerPin,LOW);
-  delayMicroseconds(4);
-  digitalWrite(TriggerPin,HIGH);
-  delayMicroseconds(10);
-  digitalWrite(TriggerPin,LOW);
-
-  duration = pulseIn(EchoPin,HIGH);
-
-  distanceCm = duration * 10/292/2;
-  return distanceCm;
+  //Valores del 2° sensor RGB
+  r2 = read16(0x0B);
+  v2 = read16(0x09);
+  a2 = read16(0x0D);
 }
+
+void imprimirRGB(){
+  //Imprimir valores del 1° RGB (en decimal)
+  Serial.print("Rojo 1: "); Serial.println(r1,DEC);
+  Serial.print("Verde 1: "); Serial.println(v1,DEC);
+  Serial.print("Azul 1: "); Serial.println(a1,DEC);
+  Serial.println();
+
+  //Imprimir valores del 2° RGB
+  Serial.print("Rojo 2: ");Serial.println(r2);
+  Serial.print("Verde 2: ");Serial.println(v2);
+  Serial.print("Azul 2: ");Serial.println(a2);
+  Serial.println("-----------------------------");
+}
+
+//Funciones para configurar y leer el 2° RGB///////////////////////
+uint16_t read16(uint8_t reg)
+{
+  uint16_t data = 0x0000;
+
+  myWire.beginTransmission(_addr);
+  myWire.write(reg);
+  myWire.endTransmission();
+  
+  myWire.beginTransmission(_addr);
+  myWire.requestFrom(_addr, (uint8_t)2); // request 2 bytes of data
+  data = myWire.read();
+  data |= (myWire.read() << 8);
+  myWire.endTransmission();
+
+  return data;
+}
+
+bool init2()
+{
+  bool ret = true;
+  uint8_t data = 0x00;
+  
+  // Start I2C
+  myWire.begin();
+  
+  // Check device ID
+  data = read8(DEVICE_ID);
+  if (data != 0x7D)
+  {
+    ret &= false;
+  }
+  
+  // Reset registers
+  ret &= reset();
+  
+  // Set to RGB mode, 10k lux, and high IR compensation
+  ret &= config2(CFG1_MODE_RGB | CFG1_10KLUX, CFG2_IR_ADJUST_HIGH, CFG_DEFAULT);
+  
+  return ret;
+}
+
+bool config2(uint8_t config1, uint8_t config2, uint8_t config3)
+{
+  bool ret = true;
+  uint8_t data = 0x00;
+  
+  // Set 1st configuration register
+  write8(CONFIG_1, config1);
+  // Set 2nd configuration register
+  write8(CONFIG_2, config2);
+  // Set 3rd configuration register
+  write8(CONFIG_3, config3);
+  
+  // Check if configurations were set correctly
+  data = read8(CONFIG_1);
+  if (data != config1)
+  {
+    ret &= false;
+  }
+  data = read8(CONFIG_2);
+  if (data != config2)
+  {
+    ret &= false;
+  }
+  data = read8(CONFIG_3);
+  if (data != config3)
+  {
+    ret &= false;
+  }
+  return ret;
+}
+
+uint8_t read8(uint8_t reg)
+{
+  myWire.beginTransmission(_addr);
+  myWire.write(reg);
+  myWire.endTransmission();
+  myWire.beginTransmission(_addr);
+  myWire.requestFrom(_addr,(uint8_t)1);
+  uint8_t data = myWire.read();
+  myWire.endTransmission();
+  
+  return data;
+}
+
+bool reset()
+{
+  uint8_t data = 0x00;
+  // Reset registers
+  write8(DEVICE_ID, 0x46);
+  // Check reset
+  data = read8(CONFIG_1);
+  data |= read8(CONFIG_2);
+  data |= read8(CONFIG_3);
+  data |= read8(STATUS);
+  if (data != 0x00)
+  {
+    return false;
+  }
+  return true;
+}
+
+void write8(uint8_t reg, uint8_t data)
+{
+  myWire.beginTransmission(_addr);
+  myWire.write(reg);
+  myWire.write(data);
+  myWire.endTransmission();
+  
+  return;
+}
+
 
